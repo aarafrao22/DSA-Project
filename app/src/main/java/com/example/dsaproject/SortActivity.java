d9ea597e630ffaa504eda82409f5b6cbc5da0eb2
aarafrao22 <aarafrao22@gmail.com>
2021-11-26 01:00:17 +0500

    DSA-Project

--- app/src/main/java/com/example/dsaproject/SortActivity.java
+++ app/src/main/java/com/example/dsaproject/SortActivity.java
@@ -5,14 +5,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class SortActivity extends AppCompatActivity implements RVClickInterface{
+public class SortActivity extends AppCompatActivity implements RVClickInterface {
 
     List<String> sortAlgo;
     RecyclerView recyclerViewS;
@@ -36,23 +38,26 @@ public class SortActivity extends AppCompatActivity implements RVClickInterface{
         sortAlgo.add("Selection Sort");
         sortAlgo.add("Bubble Sort");
 
-        itemAdapter = new ItemAdapter(sortAlgo,this);
+        itemAdapter = new ItemAdapter(sortAlgo, this);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerViewS.setLayoutManager(linearLayoutManager);
         recyclerViewS.setAdapter(itemAdapter);
         itemAdapter.notifyDataSetChanged();
-
-
-
-
-
     }
 
     @Override
     public void onItemClick(int position) {
-//        Toast.makeText(this, sortAlgo.get(position), Toast.LENGTH_SHORT).show();
-        switch (sortAlgo.get(position)){
+
+        String[] numberList = edEntries.getText().toString().split(",");
+        Integer[] numbers = new Integer[numberList.length];
+
+        for (int i = 0; i < numberList.length; i++) {
+            numbers[i] = Integer.parseInt(numberList[i]);
+        }
+
+        switch (sortAlgo.get(position)) {
             case "Merge Sort":
+
                 //
 
             case "Quick Sort":
@@ -65,8 +70,25 @@ public class SortActivity extends AppCompatActivity implements RVClickInterface{
                 //
 
             case "Bubble Sort":
+                BubbleSort(numbers,numbers.length);
+                sortedText.setText(Arrays.toString(numbers));
+                break;
                 //
-
         }
+    }
+
+    private void BubbleSort(Integer[] numbers, int length) {
+        if (length < 2) {
+            return;
+        }
+        for (int i = 0; i < length; i++) {
+            if (numbers[i] < numbers[i + 1]) {
+                Integer temp = numbers[i];
+                numbers[i] = numbers[i + 1];
+                numbers[i + 1] = temp;
+            }
+        }
+        BubbleSort(numbers,length-1);
+
     }
 }
